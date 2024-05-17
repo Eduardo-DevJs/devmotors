7462aa4e104224db94c67ad1089cd19b5119b9e8
Eduardo-DevJs <dudev2001@gmail.com>
2024-05-17 13:09:40 -0300

    style - header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,37 @@
-import styles from "./style.module.scss"
+"use client";
+import Link from "next/link";
+import styles from "./style.module.scss";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [top, setTop] = useState(true);
+
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
+
   return (
-    <header>
-      <h2>HEADER</h2>
+    <header
+      className={`${styles.header} ${!top ? styles.fixed : styles.background}`}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href={"/"}>Dev Motors</Link>
+          </div>
+          <nav className={styles.nav}>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/#servicos"}>servicoes</Link>
+            <Link href={"/#contatos"}>Contatos</Link>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
