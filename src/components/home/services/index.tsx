c5c1c9a4c16b37dc641f53496a28282862822fd9
Eduardo-DevJs <dudev2001@gmail.com>
2024-05-18 12:21:53 -0300

    style - estilizandoa area sobre

--- src/components/home/services/index.tsx
+++ src/components/home/services/index.tsx
@@ -4,20 +4,39 @@ import Image from "next/image";
 
 export default function Services({ object }: HomeProps) {
   return (
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h1 className={styles.title}>Sobre</h1>
-        <p>{object.metadata.about.description}</p>
-      </article>
-      <div className={styles.bannerAbout}>
-        <Image
-          className={styles.imageAbout}
-          alt="Imagem lustrativa da empresa"
-          fill={true}
-          quality={100}
-          src={object.metadata.about.banner.url}
-        />
-      </div>
-    </section>
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagem lustrativa da empresa"
+            fill={true}
+            quality={100}
+            src={object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+      <h2 className={styles.servicesTitle}>Conheça nosso serviços</h2>
+      <section className={styles.services}>
+        {object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.services}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt="Imagem do serviço"
+                fill={true}
+                quality={100}
+                src={service.image.url}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
